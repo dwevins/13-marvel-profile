844df382c1cb633bc1ebaf281ed9b78654b575a2
Danny Evins <dcastkid@gmail.com>
2016-07-11 14:54:55 -0500

    perform fetch, instance object, call render()

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 'use strict';
 
-export default function () {
-
+import SeriesInfoView from 'series-info-view';
+export default function (app) {
+  fetch('http://gateway.marvel.com:80/v1/public/comics?apikey=d1ba911629fc9d80d3ddb9ff8f8418d3')
+    .then((res) => res.json())
+    .then((result) => {
+      const view = new SeriesInfoView(app, result);
+      view.render();
+    });
 }
---
+++ src/series-info-view.js
@@ -0,0 +1,17 @@
+'use strict';
+
+export default class SeriesInfoView {
+  constructor(parent, data) {
+    const elements = {
+      image: '',
+      title: '',
+      startDate: '',
+      endDate: '',
+      creatorList: '',
+    }
+  }
+
+  render() {
+
+  }
+}
