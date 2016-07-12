eb324567004e54c650cbdd57d0f823510314cb12
Danny Evins <dcastkid@gmail.com>
2016-07-12 09:55:19 -0500

    write populateListItem

--- src/character-info-view.js
+++ src/character-info-view.js
@@ -18,7 +18,12 @@ export default class CharacterInfoView {
   }
 
   populateListItem(element, character) {
-
+    const imgBox = document.createElement('div');
+    const nameBox = document.createElement('div');
+    imgBox.className = 'characters__image';
+    nameBox.className = 'characters__name';
+    this.populateImg(imgBox, character);
+    this.populateName(nameBox, character);
   }
 
   populateImg(element, character) {
