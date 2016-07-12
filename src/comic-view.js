80013d8c9a97aab31156d8c53a4fce1e7cfe5d71
Danny Evins <dcastkid@gmail.com>
2016-07-12 13:29:03 -0500

    write render()

--- src/comic-view.js
+++ src/comic-view.js
@@ -2,6 +2,21 @@
 
 export default class ComicView {
   constructor(parent, object) {
+    this.comicArray = object.data.results.split(0, 3);
 
+    this.elements = {
+      comicList: parent.querySelector('.comics__list'),
+    };
   }
+
+  render() {
+    this.elements.comicList.innerHTML = '';
+    this.comicArray.forEach((comic) => {
+      const li = document.createElement('li');
+      li.className = 'comics__list--item';
+      this.populateListItem(li, comic);
+      this.populateList(li);
+    });
+  }
+
 }
