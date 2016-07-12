1d67038ff40b7772a3e4ac3dc19ac289121ac185
Danny Evins <dcastkid@gmail.com>
2016-07-12 09:41:24 -0500

    working on filling in characters section

--- src/character-info-view.js
+++ src/character-info-view.js
@@ -1,5 +1,35 @@
 'use strict';
 
 export default class CharacterInfoView {
+  constructor(parent, object) {
+    this.characterArray = object.data.results;
 
+    this.elements = {
+      characterList: parent.querySelector('.character__list'),
+    };
+  }
+
+  render() {
+    this.characterArray.forEach((character) => {
+      const li = document.createElement('li');
+      this.populateListItem(li, character);
+      this.populateList(li);
+    });
+  }
+
+  populateListItem(element, character) {
+
+  }
+
+  populateImg(element, character) {
+
+  }
+
+  populateName(element, character) {
+
+  }
+
+  populateList(element) {
+
+  }
 }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
 'use strict';
 
 import SeriesInfoView from 'series-info-view';
+import CharacterInfoView from 'character-info-view';
 export default function (app) {
   const leftPane = app.querySelector('.left-pane__content');
-  const characters = app.querySelector('characters');
-  const comics = app.querySelector('comics');
+  const characters = app.querySelector('.characters');
+  const comics = app.querySelector('.comics');
   fetch('http://gateway.marvel.com:80/v1/public/series/9856?apikey=d1ba911629fc9d80d3ddb9ff8f8418d3')
     .then((res) => res.json())
     .then((result) => {
-      console.log(result);
       const view = new SeriesInfoView(leftPane, result);
       view.render();
+    });
+
+  fetch('http://gateway.marvel.com:80/v1/public/series/9856/characters?apikey=d1ba911629fc9d80d3ddb9ff8f8418d3')
+    .then((res) => res.json())
+    .then((result) => {
+      console.log(result);
+      const view = new CharacterInfoView(characters, result);
+
     });
 }
